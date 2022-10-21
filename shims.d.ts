8b89aba40169842b2897df43a3f542cb743ba589
zhang-tang <80501060+zhang-tang@users.noreply.github.com>
2022-10-21 16:48:24 +0800

    更新 pxt.json, shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -4,7 +4,7 @@
 
     //% color=50 weight=80
     //% icon="\uf1eb"
-    declare namespace i2c {
-    }
-    
-    // Auto-generated. Do not edit. Really.
+declare namespace i2c {
+}
+
+// Auto-generated. Do not edit. Really.
